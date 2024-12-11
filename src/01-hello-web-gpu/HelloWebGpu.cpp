b2dc05b435a84b4c613fcc006fa3e2c4a84dba12
kyle <wanglikang@bigheadbros.cn>
2024-12-11 18:35:04 +0800

    Hello Web GPU

---
+++ src/01-hello-web-gpu/HelloWebGpu.cpp
@@ -0,0 +1,57 @@
+//
+// Created by DELL on 2024/12/11.
+//
+
+#include <iostream>
+
+#include <webgpu/webgpu_cpp.h>
+
+
+int main()
+{
+    std::cout << "Hello Web GPU" << std::endl;
+
+    wgpu::InstanceDescriptor instanceDescriptor{};
+    instanceDescriptor.features.timedWaitAnyEnable = true;
+    wgpu::Instance instance = wgpu::CreateInstance(&instanceDescriptor);
+    if (instance == nullptr) {
+        std::cerr << "Instance creation failed!\n";
+        return EXIT_FAILURE;
+    }
+    // Synchronously request the adapter.
+    wgpu::RequestAdapterOptions options = {};
+    wgpu::Adapter adapter;
+    wgpu::RequestAdapterCallbackInfo callbackInfo = {};
+    callbackInfo.nextInChain = nullptr;
+    callbackInfo.mode = wgpu::CallbackMode::WaitAnyOnly;
+    callbackInfo.callback = [](WGPURequestAdapterStatus status,
+                               WGPUAdapter adapter, WGPUStringView message,
+                               void *userdata) {
+        if (status != WGPURequestAdapterStatus_Success) {
+            // std::cerr << "Failed to get an adapter:" << message;
+            return;
+        }
+        *static_cast<wgpu::Adapter *>(userdata) = wgpu::Adapter::Acquire(adapter);
+    };
+    callbackInfo.userdata = &adapter;
+    instance.WaitAny(instance.RequestAdapter(&options, callbackInfo), UINT64_MAX);
+    if (adapter == nullptr) {
+        std::cerr << "RequestAdapter failed!\n";
+        return EXIT_FAILURE;
+    }
+
+    wgpu::DawnAdapterPropertiesPowerPreference power_props{};
+
+    wgpu::AdapterInfo info{};
+    info.nextInChain = &power_props;
+
+    adapter.GetInfo(&info);
+    std::cout << "VendorID: " << std::hex << info.vendorID << std::dec << "\n";
+    // std::cout << "Vendor: " << info.vendor << "\n";
+    // std::cout << "Architecture: " << info.architecture << "\n";
+    // std::cout << "DeviceID: " << std::hex << info.deviceID << std::dec << "\n";
+    // std::cout << "Name: " << info.device << "\n";
+    // std::cout << "Driver description: " << info.description << "\n";
+
+    return 0;
+}
